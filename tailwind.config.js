b62016288c88d282fe69eb77d8bdba1e4a498ea8
CodingGupta <117964173+CodingGupta@users.noreply.github.com>
2024-05-29 15:12:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish : ["Mulish","sans-serif"],
+        
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightlue: "#2b84ea",
+        lightblue300: "#4b94ed",
+        lightblue500: "#0b72e7",
+      },
+    },
+  },
+  plugins: [],
+}
